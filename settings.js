8027655d62542fb756103c608341fe626d6e477f
Matata6647 <masindematata0@gmail.com>
2025-02-08 00:24:20 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -10,8 +10,8 @@ const fs = require('fs')
 const chalk = require('chalk')
 
 //contact details
-global.ownernomer = "910000000000"
-global.ownername = "🐛Tech God"
+global.ownernomer = "254796832122"
+global.ownername = "🐛Bonnie Tech"
 global.ytname = "YT: Tech-God"
 global.socialm = "GitHub: techgod143"
 global.location = "India, Up, Rampur"
